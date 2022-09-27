e75b8ba23429439bb4483b149f1d4cee3294f77f
김형석 <70968732+kim-hyeungsuk@users.noreply.github.com>
2022-09-27 22:52:11 +0900

    Update DeletePostController.java

--- semiproject/src/controller/DeletePostController.java
+++ semiproject/src/controller/DeletePostController.java
@@ -13,10 +13,6 @@ public class DeletePostController implements Controller {
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		HttpSession session=request.getSession(false);
-//		if(session==null||session.getAttribute("mvo")==null||
-//				request.getMethod().equals("POST")==false){
-//			return "redirect:index.jsp";
-//		}
 		MemberVO mvo = (MemberVO)session.getAttribute("mvo");
 		String no=request.getParameter("no");
 		System.out.println("number:"+no);
